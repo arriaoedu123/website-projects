200f6ef8ff9317c31f383f7c51fd3b9299295bc7
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-07-10 20:53:18 -0300

    Update FILES

--- project01-hardware-store/script.js
+++ project01-hardware-store/script.js
@@ -1,19 +1,27 @@
 /* 
 # Creator: Arreaum
 # GitHub: https://github.com/arriaoedu123/
-# Creation date: 29/03/2021
-# Update date: 30/03/2021
+# Creation date: 10/03/2021
+# Update date:
 # Version: 1.0
 */
-
 const arrows = document.querySelectorAll(".arrow");
 const gameLists = document.querySelectorAll(".game-list");
+let move = 0;
 
 arrows.forEach((arrow, i) => {
+    const itemNumber = gameLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
     arrow.addEventListener("click", () => {
-    	gameLists[i].style.transform = "translateX(-100px)"
-    })
-})
+        const ratio = Math.floor(window.innerWidth / 250);
+        clickCounter++;
+        if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+            gameLists[i].style.transform = `translateX(${move-=250}px)`
+        } else {
+            gameLists[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+            move = 0;
+        }
+    });
 
-
-
+});
